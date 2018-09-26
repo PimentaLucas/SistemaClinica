a9afb328af39bc11d2a705cced3acee76e1f9ec5
Lucas Pimenta <lucas.pimenta@uniriotec.br>
2018-09-26 14:24:34 -0300

    Tabela Cliente mapeada e dependencias configuradas

--- SistemaClinica/src/Entidades/Cliente.java
+++ SistemaClinica/src/Entidades/Cliente.java
@@ -1,7 +1,13 @@
 package Entidades;
 
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+
+@Entity
 public class Cliente {
 
+		
 		private String nome;
 		private String cpf_client;
 		private String telefone;
@@ -10,41 +16,58 @@ public class Cliente {
 		private String email;
 		
 		
+		@Column(name="NOME",length=150)
 		public String getNome() {
 			return nome;
 		}
-		public void setNome(String nome) {
-			this.nome = nome;
-		}
+		
+		@Id
+		@Column(name="CPF",length=11)
 		public String getCpf_client() {
 			return cpf_client;
 		}
-		public void setCpf_client(String cpf_client) {
-			this.cpf_client = cpf_client;
-		}
+		
+		@Column(name="TELEFONE")
 		public String getTelefone() {
 			return telefone;
 		}
-		public void setTelefone(String telefone) {
-			this.telefone = telefone;
-		}
+		
+		@Column(name="CELULAR")
 		public String getCelular() {
 			return celular;
 		}
-		public void setCelular(String celular) {
-			this.celular = celular;
-		}
+		
+		@Column(name="ENDERECO")
 		public String getEndereco() {
 			return endereco;
 		}
-		public void setEndereco(String endereco) {
-			this.endereco = endereco;
-		}
+		
+		@Column(name="EMAIL")
 		public String getEmail() {
 			return email;
 		}
+		
 		public void setEmail(String email) {
 			this.email = email;
 		}
 		
+		public void setNome(String nome) {
+			this.nome = nome;
+		}
+		
+		public void setEndereco(String endereco) {
+			this.endereco = endereco;
+		}
+		
+		public void setCelular(String celular) {
+			this.celular = celular;
+		}
+		
+		public void setTelefone(String telefone) {
+			this.telefone = telefone;
+		}
+		
+		public void setCpf_client(String cpf_client) {
+			this.cpf_client = cpf_client;
+		}
 }
